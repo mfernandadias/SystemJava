cce2092ad3fc14fa38ff1c289d6c8248388c5a90
Fernanda Dias <mariadosantodias@gmail.com>
2025-04-21 01:43:11 -0300

    program

--- src/main/java/projetoCadastroUsuario/Program.java
+++ src/main/java/projetoCadastroUsuario/Program.java
@@ -1,5 +1,61 @@
 package projetoCadastroUsuario;
+import projetoCadastroUsuario.model.Usuario;
+import projetoCadastroUsuario.file.ArquivoUtils;
+import projetoCadastroUsuario.service.UsuarioService;
+
+import java.util.Scanner;
 
 public class Program {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        UsuarioService usuarioService = new UsuarioService();
+        String caminhoArquivo = "usuarios.txt";
 
+        while(true){
+            System.out.println("1. Cadastrar");
+            System.out.println("2. Listar");
+            System.out.println("3. Atualizar ");
+            System.out.println("4. Excluir");
+            System.out.println("5. Salvar arquivo");
+            System.out.println("6. Sair");
+            System.out.println("Escolha: ");
+            int opcao = scan.nextInt();
+            scan.nextLine();
+
+            switch(opcao){
+                case 1:
+                    System.out.println("Nome: ");
+                    String nome = scan.nextLine();
+                    System.out.println("Telefone: ");
+                    String telefone = scan.nextLine();
+                    System.out.println("Cargo: ");
+                    String cargo = scan.nextLine();
+                    break;
+                case 2:
+                    int i = 0;
+                    for (Usuario u : usuarioService.listarUsuarios()) {
+                        System.out.println(i + " - " + u);
+                        i++;
+                    }
+                    break;
+                case 3:
+                    System.out.println("ìndice para atualizar: ");
+                    int index = scan.nextInt();
+                    scan.nextLine();
+                    System.out.print("Novo nome: ");
+                    String novoNome = scan.nextLine();
+                    System.out.print("Novo telefone: ");
+                    String novoTel = scan.nextLine();
+                    System.out.print("Novo cargo: ");
+                    String novoCargo = scan.nextLine();
+                    Usuario atual = new Usuario(novoNome, novoTel, novoCargo);
+                    usuarioService.atualizarUsuario(index, atual);
+                    break;
+                case 4:
+                    System.out.println("Índice para excluir")
+            }
+        }
+
+
+    }
 }
